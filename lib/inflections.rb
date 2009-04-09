589d29ff4399c6bc827db35191fb3ff109f94321
Abhishek Parolkar <abhishek@parolkar.com>
2009-04-09 15:46:56 +0530

    Added inflections

--- lib/inflections.rb
+++ lib/inflections.rb
@@ -5,13 +5,14 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.past_tense /e$/,'ed'
   inflect.past_tense /t$/,'ted'
   inflect.past_tense /g$/,'gged'
-  inflect.past_tense /ight$/,'aught'
-  inflect.past_tense "buy",'baught'  
+  inflect.past_tense /ight$/,'ought'
+  inflect.past_tense "buy",'bought'  
   inflect.past_tense "sell",'sold'
   inflect.past_tense "is",'was'
   inflect.past_tense "are",'were'
   inflect.past_tense "teach",'taught'
   inflect.past_tense "feel",'felt'  
+  inflect.past_tense "light",'lit'
 
  
 
